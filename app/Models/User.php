d500a4f59a4724798b37e6f167fb9ccd18618e35
novapc <yandex74@yandex.ru>
2024-09-06 14:11:53 +0300

    set users to db, test api/passport/token success

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
